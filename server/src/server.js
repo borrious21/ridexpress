5e2e96c3233d94f30b69f84251fa6e9edc025708
Devit-rai <davidrai806@gmail.com>
2025-08-13 14:48:49 +0545

    ..

---
+++ server/src/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+app.get("/",(req, res) =>{
+
+})
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>{
+    console.log(`Server is running on ${PORT}`)
+
+});
