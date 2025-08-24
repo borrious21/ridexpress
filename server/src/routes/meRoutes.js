566f7d20cdd63db97886655ceee7f186b89e24cf
Parikshit Gautam <parikshit10gautam@gmail.com>
2025-08-24 17:17:37 +0545

    updated

--- server/src/routes/meRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { requireAuth } from '../middleware/auth.js';
-import { me } from '../controller/loginAuthController.js';
-
-const router = Router();
-
-router.get('/me', requireAuth, me);
-
-export default router;
