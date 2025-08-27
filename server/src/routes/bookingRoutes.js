b2406bac387632f3e981488bbbf47a88d2e2d5b3
Rohitshah507 <shahaaditya1111@gmail.com>
2025-08-27 23:28:07 +0545

    Updated

--- server/src/routes/bookingRoutes.js
+++ server/src/routes/bookingRoutes.js
@@ -1,14 +1,16 @@
 import express from "express";
 import bookingController from "../controller/bookingController.js";
 import auth from "../middleware/auth.js";
+import roleBasedAuth from "../middleware/roleBasedAuth.js";
+import { ADMIN } from "../constants/roles.js";
 
 const router = express.Router();
 
 //URL: /api/booking
-router.get("/", bookingController.getBooking);
+router.get("/", roleBasedAuth[ADMIN], bookingController.getBooking);
 
 router.post("/", auth, bookingController.createBooking);
 
-router.delete("/:id", bookingController.deleteBooking);
+router.delete("/:id", roleBasedAuth[ADMIN], bookingController.deleteBooking);
 
 export default router;
