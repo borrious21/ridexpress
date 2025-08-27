20b06f19db5f1f9b1ce367af5df301b3fd9146c7
Rohitshah507 <shahaaditya1111@gmail.com>
2025-08-27 20:10:12 +0545

    Updated

--- server/src/controller/authController.js
+++ server/src/controller/authController.js
@@ -1,17 +1,19 @@
 import authService from "../services/authServices.js";
-import jwt from "jsonwebtoken";
-import { createJWT, verifyJWT } from "../utils/tokens.js";
+import { createJWT } from "../utils/tokens.js";
 
 const signup = async (req, res) => {
   const input = req.body;
 
   try {
+    
     if (!input.password) {
       return res.status(400).json({ message: "Password is required" });
     }
+
     if (!input.confirmPassword) {
       return res.status(400).json({ message: "Confirm Password is required" });
     }
+
     if (input.password !== input.confirmPassword) {
       return res.status(400).json({ message: "Passwords do not match" });
     }
@@ -20,20 +22,17 @@ const signup = async (req, res) => {
 
     const authToken = createJWT(data);
 
-    console.log(user);
-
     res.cookie("authToken", authToken, { maxAge: 900000 * 1000 });
 
-    return res
-      .status(201)
-      .json({ message: "User registered successfully", user });
+    res.status(201).json({ message: "User registered successfully", user });
   } catch (error) {
-    return res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message });
   }
 };
 
 const login = async (req, res) => {
   const input = req.body;
+
   try {
     if (!input) {
       return res.status(400).json({ message: "Required fields are required" });
@@ -42,6 +41,7 @@ const login = async (req, res) => {
     if (!input.email) {
       return res.status(400).json({ message: "Email is required" });
     }
+
     if (!input.password) {
       return res.status(400).json({ message: "Password is required" });
     }
@@ -50,13 +50,11 @@ const login = async (req, res) => {
 
     const authToken = createJWT(user);
 
-    console.log(user);
-
     res.cookie("authToken", authToken, { maxAge: 900000 * 1000 });
 
-    return res.status(200).json({ message: "Login successful", user });
+    res.status(200).json({ message: "Login successful", user });
   } catch (error) {
-    return res
+    res
       .status(error.statusCode || 500)
       .json({ message: error.message || "Server error" });
   }
