9b27e973b11bc11fc0a803972a0f1202f381cbdd
Parikshit Gautam <parikshit10gautam@gmail.com>
2026-05-25 07:17:57 +0545

    Add vehicle module with upload and cloudinary integration

--- server/src/routes/vehicleRoutes.js
+++ server/src/routes/vehicleRoutes.js
@@ -1,30 +1,19 @@
 import express from "express";
+import multer from "multer";
 import vehicleController from "../controller/vehicleController.js";
 import roleBasedAuth from "../middleware/roleBasedAuth.js";
 import { ADMIN } from "../constants/roles.js";
 import auth from "../middleware/auth.js";
 
 const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
 
-//URL:/api/vehicles"
+// URL: /api/vehicles
 router.get("/", vehicleController.getVehicles);
-
 router.get("/:id", vehicleController.getVehiclesById);
 
-router.post("/", auth, roleBasedAuth(ADMIN), vehicleController.createVehicle);
-
-router.put(
-  "/:id",
-  auth,
-  roleBasedAuth(ADMIN),
-  vehicleController.updatedVehicles
-);
-
-router.delete(
-  "/:id",
-  auth,
-  roleBasedAuth(ADMIN),
-  vehicleController.deleteVehicles
-);
+router.post("/", auth, roleBasedAuth(ADMIN), upload.array("photos", 10), vehicleController.createVehicle);
+router.put("/:id", auth, roleBasedAuth(ADMIN), upload.array("photos", 10), vehicleController.updatedVehicles);
+router.delete("/:id", auth, roleBasedAuth(ADMIN), vehicleController.deleteVehicles);
 
 export default router;
