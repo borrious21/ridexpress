5b25e03e93b6e68a0c657236d8ae101ecd6715a4
Rohitshah507 <shahaaditya1111@gmail.com>
2026-05-26 13:59:04 +0545

    Deleting the unwanted code

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -5,10 +5,7 @@ dotenv.config();
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log('MongoDB Connected');
   } catch (err) {
     console.error('MongoDB connection failed:', err);
